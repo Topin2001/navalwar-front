72106eb1958ce785fa9a7b355942e146bc08e6c5
Topin2001 <thomas@galpin.fr>
2023-01-29 23:45:44 +0100

    end of day commit, add vessel selection

--- src/Grid.js
+++ src/Grid.js
@@ -14,19 +14,36 @@ const Grid = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const [selectedShip, setSelectedShip] = useState(null);
 
-  const handleClick = (row, col) => {
-    console.log(row, col);
-    let newGrid = [...grid];
-    if (grid[row][col] === 0) {
-        newGrid[row][col] = 1;
-    }else{
-        newGrid[row][col] = 0;
-    }
-    setGrid(newGrid);
+  const handleSelectShip = (ship) => {
+    setSelectedShip(ship)
+    console.log(ship, selectedShip);
   };
 
+  const handlePlaceShip = (row, col) => {
+    console.log(selectedShip);
+    if (!selectedShip) {
+      alert('Please select a ship to place');
+      return;
+    }
+
+    let newGrid = [...grid];
+    newGrid[row][col] = 1;
+    setGrid(newGrid);
+    setSelectedShip(null);
+  };
+
+
   return (
+    <div>
+      <h2>Placement des bateaux</h2>
+      <button onClick={() => handleSelectShip('Destroyer')}>Destroyer</button>
+      <button onClick={() => handleSelectShip('Cruiser')}>Cruiser</button>
+      <button onClick={() => handleSelectShip('Submarine')}>Submarine</button>
+      <button onClick={() => handleSelectShip('Battleship')}>Battleship</button>
+      <button onClick={() => handleSelectShip('Carrier')}>Carrier</button>
+      <p>Selected Ship: {selectedShip}</p>
     <div className="grid">
       {grid.map((row, rowIndex) => (
         <div className="row" key={rowIndex}>
@@ -34,13 +51,16 @@ const Grid = () => {
             <div
               className={`col ${col === 1 ? "hit" : ""}`}
               key={colIndex}
-              onClick={() => handleClick(rowIndex, colIndex)}
+              onClick={() => handlePlaceShip(rowIndex, colIndex)}
             ></div>
           ))}
         </div>
       ))}
     </div>
+    </div>
   );
+
 };
+
 
 export default Grid;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Grid from './Grid'
-import ShipPlacementButtons from './ShipPlacementButtons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Grid/>
-    <ShipPlacementButtons/>
   </React.StrictMode>
 );
 
