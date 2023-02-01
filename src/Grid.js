d2dcd85966b7efa176adfdff9afd4391ba274b91
Topin2001 <thomas@galpin.fr>
2023-02-01 17:05:05 +0100

    square fix and selection less agressive

--- src/Grid.js
+++ src/Grid.js
@@ -16,47 +16,52 @@ const Grid = () => {
   ]);
   const [selectedShip, setSelectedShip] = useState(null);
 
+  const ship = ['Destroyer', 'Cruiser', 'Submarine', 'Battleship', 'Carrier'];
+
   const handleSelectShip = (ship) => {
-    setSelectedShip(ship)
-    console.log(ship, selectedShip);
+    const msg_error = document.getElementById("error_msg")
+    msg_error.innerHTML = ""
+    setSelectedShip(ship);
   };
 
   const handlePlaceShip = (row, col) => {
-    console.log(selectedShip);
-    if (!selectedShip) {
-      alert('Please select a ship to place');
-      return;
+    const newGrid = [...grid];
+
+    if (grid[row][col] !== 0) {
+      newGrid[row][col] = 0;
+    } else {
+      if (!selectedShip) {
+        const msg_error = document.getElementById("error_msg")
+        msg_error.innerHTML = "Merci de choisir un bateau à placer !"
+        return;
+      }
+      newGrid[row][col] = 1;
+      setSelectedShip(null);
     }
-
-    let newGrid = [...grid];
-    newGrid[row][col] = 1;
     setGrid(newGrid);
-    setSelectedShip(null);
   };
-
 
   return (
     <div>
       <h2>Placement des bateaux</h2>
-      <button onClick={() => handleSelectShip('Destroyer')}>Destroyer</button>
-      <button onClick={() => handleSelectShip('Cruiser')}>Cruiser</button>
-      <button onClick={() => handleSelectShip('Submarine')}>Submarine</button>
-      <button onClick={() => handleSelectShip('Battleship')}>Battleship</button>
-      <button onClick={() => handleSelectShip('Carrier')}>Carrier</button>
-      <p>Selected Ship: {selectedShip}</p>
-    <div className="grid">
-      {grid.map((row, rowIndex) => (
-        <div className="row" key={rowIndex}>
-          {row.map((col, colIndex) => (
-            <div
-              className={`col ${col === 1 ? "hit" : ""}`}
-              key={colIndex}
-              onClick={() => handlePlaceShip(rowIndex, colIndex)}
-            ></div>
-          ))}
-        </div>
+      {ship.map(navire => (
+        <button key={navire} onClick={() => handleSelectShip(navire)}>{navire}</button>
       ))}
-    </div>
+      <p>Selected Ship: {selectedShip}</p>
+      <p id="error_msg"></p>
+      <div className="grid">
+        {grid.map((row, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {row.map((col, colIndex) => (
+              <div
+                className={`col ${col !== 0 ? "hit" : ""}`}
+                key={colIndex}
+                onClick={() => handlePlaceShip(rowIndex, colIndex)}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 
