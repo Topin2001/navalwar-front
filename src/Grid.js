a819e01aab21f7ec4a9517ed9a1f527ef7b64e05
Topin2001 <wonodoou00@gmail.com>
2023-02-24 14:33:06 +0100

    add login page

--- src/Grid.js
+++ src/Grid.js
@@ -1,3 +1,4 @@
+import Login from "./Login";
 import GridPlacement from "./GridPlacement";
 import GridPlay from "./GridPlay";
 import VictoryPage from "./Win";
@@ -5,11 +6,12 @@ import LoosePage from "./Loose";
 import React, { useState } from "react";
 
 const Grid = () => {
-  const [Etat, setEtat] = useState(["Placement"]);
+  const [Etat, setEtat] = useState(["Login"]);
 
   const grid_size = 10;
-
-  if (Etat == "Placement") {
+  if (Etat == "Login") {
+    return <Login setEtat={setEtat} />;
+  } else if (Etat == "Placement") {
     return <GridPlacement grid_size={grid_size} setEtat={setEtat} />;
   } else if (Etat == "Jeux") {
     return <GridPlay grid_size={grid_size} setEtat={setEtat} />;
---
+++ src/Login.js
@@ -0,0 +1,58 @@
+import "./Login.css "
+import React, { useState } from "react";
+
+const Login = ({ setEtat }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setEtat("Placement");
+  };
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    setEtat("Placement");
+  };
+
+  return (
+    <div className="login">
+      <h1>{isRegistering ? "Register" : "Login"}</h1>
+      <form onSubmit={isRegistering ? handleRegister : handleLogin}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <br />
+        <button type="submit">{isRegistering ? "Register" : "Login"}</button>
+      </form>
+      <button onClick={() => setIsRegistering(!isRegistering)}>
+        {isRegistering
+          ? "Already have an account? Login"
+          : "Don't have an account? Register"}
+      </button>
+    </div>
+  );
+};
+
+export default Login;
